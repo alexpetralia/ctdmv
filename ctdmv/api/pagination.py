87dfec518195bad77b16043684cfa2f0bfa89743
Alex Petralia <alex@alexpetralia.com>
2019-10-24 20:44:17 -0400

    Reducing pagination size, too large seems to crash app

--- ctdmv/api/pagination.py
+++ ctdmv/api/pagination.py
@@ -3,5 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 200
-    max_page_size = 100000
+    max_page_size = 10000
     page_size_query_param = "page_size"
